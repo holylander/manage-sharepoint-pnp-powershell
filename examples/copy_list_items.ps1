089271a1ea793d37cf9fcc015d1e0bea0871d77b
Alberto Suarez Caballero <Alberto.SuarezCaballero@arris.com>
2019-05-31 11:44:17 +0200

    better copylistItems example

--- examples/copy_list_items.ps1
+++ examples/copy_list_items.ps1
@@ -7,13 +7,16 @@ Write-Host $("`nMSG: script Started----`n");
 
 checkModules;
 $source_site="";  ## feel free to to prefill this var with the target URL if needed.
-$site1_connection = connectToSite -srcSite $source_site -stored_credential spPremises;
+$site1_connection = connectToSite -srcSite $source_site -stored_credential [stored_credential]
 
-$fields="ID","Title"
+$fields="ID","Title","owner","usedBy"
 $list_name=""  # feel free to to prefill this var with the target list name if needed.
 $filter=$null  # Filter example: {$_.FieldValues.ID -eq 5} 
-
 $list_items= getListItems -site $site1_connection -fields $fields -list_name $list_name -filter $filter
-$copy_result=copyListsItems -site $site1_connection -item_Collection $list_items -list_name $list_name
+
+$target_site=$source_site
+$site2_connection = connectToSite -srcSite $target_site -stored_credential [stored_credential]
+$list_name="" 
+$copy_result=copyListsItems -site $site2_connection -item_Collection $list_items -list_name $list_name
 
 Write-Host $("`n----MSG: script Ended: `n");
