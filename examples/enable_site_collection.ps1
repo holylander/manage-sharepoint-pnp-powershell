2b7fab537d826e65ef4de06b9eae8f0d037e5ff0
Alberto Suarez Caballero <alberto.suarez@viacelere.local>
2019-07-11 09:29:34 +0200

    spfx

---
+++ examples/enable_site_collection.ps1
@@ -0,0 +1,12 @@
+
+Import-Module -Force $PSScriptRoot\..\src\resources\SP_tools.psm1;
+Import-Module -Force $PSScriptRoot\..\src\connection\SP_connection_Manager.psm1;
+
+
+Write-Host $("`nMSG: script Started----`n");
+
+#checkModules;
+$source_site="";  ## feel free to to prefill this var with the target URL if needed.
+$site1_connection = connectToSite -srcSite $source_site -stored_credential spOnline;
+Add-PnPSiteCollectionAppCatalog  -Site $source_site
+Write-Host $("`n----MSG: script Ended: `n");
